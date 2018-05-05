7221b7ac01a016742bf55ed0dcb44a0a83482706
Tim Chen <chengt@gmail.com>
2018-05-05 16:42:37 -0400

    Enhance fix for #1240 using @jeffgbutler always choose if single constructor suggestion Also use type registry instead of primitivetypes checking as it's more extensive and allows for better constructor mapping behavior.

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.autoconstructor;
 
@@ -22,12 +22,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.Reader;
 import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
 
 public class AutoConstructorTest {
   private static SqlSessionFactory sqlSessionFactory;
@@ -41,7 +42,7 @@ public class AutoConstructorTest {
 
     // populate in-memory database
     BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
-            "org/apache/ibatis/autoconstructor/CreateDB.sql");
+        "org/apache/ibatis/autoconstructor/CreateDB.sql");
   }
 
   @Test
@@ -49,7 +50,7 @@ public class AutoConstructorTest {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       final Object subject = mapper.getSubject(1);
-      Assert.assertNotNull(subject);
+      assertNotNull(subject);
     }
   }
 
@@ -58,14 +59,6 @@ public class AutoConstructorTest {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       mapper.getSubjects();
-    }
-  }
-
-  @Test
-  public void wrapperSubject() {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      verifySubjects(mapper.getWrapperSubjects());
     }
   }
 
@@ -85,8 +78,16 @@ public class AutoConstructorTest {
     }
   }
 
+  @Test
+  public void extensiveSubject() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+      verifySubjects(mapper.getExtensiveSubject());
+    }
+  }
+
   private void verifySubjects(final List<?> subjects) {
-    Assert.assertNotNull(subjects);
+    assertNotNull(subjects);
     Assertions.assertThat(subjects.size()).isEqualTo(3);
   }
 }
